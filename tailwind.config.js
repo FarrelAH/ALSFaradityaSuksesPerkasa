8e90cdbeaa627d71cdda8a552f1df07f11c3a852
MoonLGH <MoonLMC1212@gmail.com>
2022-05-31 05:35:58 +0000

    colors not extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
 module.exports = {
   content: ['./src/*.html'],
   safelist: [
-    '!bg-blue-jumbotron',
-    'bg-blue-jumbotron',
     'carousel-item-next',
     'carousel-item-left',
     'carousel-item-prev,',
     'carousel-item-right'
   ],
   theme: {
-    extend: {
-      colors: {
-        'blue-jumbotron': '#003556',
-      },
-    }
+    colors: {
+      'blue-jumbotron': '#003556',
+    },
   },
   variants: {},
   plugins: [require("daisyui")],
