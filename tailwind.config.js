e312ea8285b8caa5200075984c735804bab4be9b
MoonLGH <MoonLMC1212@gmail.com>
2022-05-31 05:45:16 +0000

    wtf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   content: ['./src/*.html'],
   safelist: [
-    'carousel-item-next',
-    'carousel-item-left',
+    '!bg-blue-jumbotron',
+    'bg-blue-jumbotron',
+    'carousel-item-next carousel-item-left',
     'carousel-item-prev,',
     'carousel-item-right'
   ],
   theme: {
+    extend: {
+      colors: {
+        'blue-jumbotron': '#003556',
+      },
+    }
   },
   variants: {},
   plugins: [require("daisyui")],
