2b113178ca421d7b359d36be6b7c66feeb873d38
MoonLGH <MoonLMC1212@gmail.com>
2022-05-31 05:32:22 +0000

    revert and use safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/*.html'],
+  safelist: [
+    '!bg-blue-jumbotron',
+    'bg-blue-jumbotron',
+    'carousel-item-next',
+    'carousel-item-left',
+    'carousel-item-prev,',
+    'carousel-item-right'
+  ],
   theme: {
     extend: {
+      colors: {
+        'blue-jumbotron': '#003556',
+      },
     }
   },
   variants: {},
